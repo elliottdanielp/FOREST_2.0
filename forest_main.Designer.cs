969d7c0f1f339e921765600f3533198f3337cc8c
Dan <106495422+elliottdanielp@users.noreply.github.com>
2022-12-01 15:00:54 -0500

    Update main form added sales.

--- forest_main.Designer.cs
+++ forest_main.Designer.cs
@@ -28,14 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Handoff");
+            System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Sales", new System.Windows.Forms.TreeNode[] {
+            treeNode1});
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(forest_main));
+            this.treeView1 = new System.Windows.Forms.TreeView();
             this.SuspendLayout();
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(12, 12);
+            this.treeView1.Name = "treeView1";
+            treeNode1.Name = "sales_handoff";
+            treeNode1.Text = "Handoff";
+            treeNode2.Name = "Sales";
+            treeNode2.Text = "Sales";
+            this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode2});
+            this.treeView1.Size = new System.Drawing.Size(121, 426);
+            this.treeView1.TabIndex = 0;
             // 
             // forest_main
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.treeView1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "forest_main";
             this.Text = "FOREST";
@@ -44,5 +62,7 @@
         }
 
         #endregion
+
+        private TreeView treeView1;
     }
 }
