043c291dac03f60bf53d419dd5ce49eee8d63d27
Dan <106495422+elliottdanielp@users.noreply.github.com>
2022-12-01 14:47:14 -0500

    Update to main form

--- forest_main.Designer.cs
+++ forest_main.Designer.cs
@@ -28,10 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(forest_main));
+            this.SuspendLayout();
+            // 
+            // forest_main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "forest_main";
+            this.Text = "FOREST";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
